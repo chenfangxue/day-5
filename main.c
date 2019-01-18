0e98c5c9c94559813f84fe21109720e7205f14b6
cfx <you@example.com>
2019-01-18 10:34:49 +0800

    1

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 
+struct student
+{
+    int yuwen;
+    int shuxue;
+    int yingyu;
+    char name[100];
+};
+
 int main()
 {
-
     printf("进入学生成绩管理系统\n");
+    struct student arr[1000];
+    int index = 0;
     while (1)
+
     {
         printf("1--添加学生成绩\n");
         printf("2--删除最后一个学生成绩\n");
@@ -19,7 +29,19 @@ int main()
         scanf("%d", &code);
         if (code == 1)
         {
-
+            printf("请输入学生姓名\n");
+            scanf("%s", arr[index].name);
+            printf("添加语文成绩\n");
+            scanf("%d", &arr[index].yuwen);
+            printf("添加数学成绩\n");
+            scanf("%d", &arr[index].shuxue);
+            printf("添加英语成绩\n");
+            scanf("%d", &arr[index].yingyu);
+            index++;
+            printf("添加成功，点击回车继续\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 2)
         {
@@ -27,23 +49,18 @@ int main()
         }
         if (code == 3)
         {
-            
         }
         if (code == 4)
         {
-            
         }
         if (code == 5)
         {
-            
         }
         if (code == 6)
         {
-            
         }
         if (code == 7)
         {
-            
         }
         if (code == 8)
         {
